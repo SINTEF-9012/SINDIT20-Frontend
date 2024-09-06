06d73652672a21e2102c30d9eb731724b0ab6096
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-09-06 23:30:29 +0200

    update: workspaces like dashboards

--- src/routes/workspaces/+page.svelte
+++ src/routes/workspaces/+page.svelte
@@ -1,42 +1,81 @@
 <script lang="ts">
-    import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 	import { selectedWorkspace } from '$lib/stores';
 
     	// TODO: Implement workspaces in the form of a list of KGs to select from.
-	const workspaces: string[] = ['workspace-1', 'workspace-2', 'workspace-3'];
+	const workspaces: string[] = ['workspace-1', 'workspace-2', 'workspace-3', 'yolo', 'Han solo', 'Chewbacca'];
 	let searchQuery = '';
 	let _selectedWorkspace = '';
 	let filteredWorkspaces: string[] = [];
-	$: filteredWorkspaces = workspaces.filter(workspace => workspace.toLowerCase().includes(searchQuery.toLowerCase())).slice(0, 5);
+	$: {
+		if (searchQuery === '') {
+            filteredWorkspaces = workspaces;
+		} else {
+            filteredWorkspaces = workspaces.filter(workspace => workspace.toLowerCase().includes(searchQuery.toLowerCase())).slice(0, 5);
+        }
+	}
 	$: {
 		if (_selectedWorkspace) {
 			selectedWorkspace.set(_selectedWorkspace);
 		}
 	}
+
+	function selectWorkspace(workspace: string) {
+		_selectedWorkspace = workspace;
+	}
+
+	function onCreateNewWorkspace() {
+		console.log('create new workspace');
+	}
 </script>
 
 <div>
-    <h1>Workspaces</h1>
-    <div class="workspaces flex flex-col columns-1 variant-primary">
-		<input type="text" bind:value={searchQuery} placeholder="Search workspaces" />
-		<ListBox class="workspace-list">
-			{#each filteredWorkspaces as workspace}
-				<ListBoxItem
-					bind:group={_selectedWorkspace}
-					name={workspace} value={workspace}>{workspace}
-				</ListBoxItem>
-			{/each}
-		</ListBox>
-	</div>
+    <h1 class="text-4xl">Workspaces</h1>
+    <br>
+    <header class="flex grid-flow-row columns-3 gap-2">
+        <input type="text" bind:value={searchQuery} placeholder="Search workspaces..." />
+        <button class="btn variant-ghost-primary"
+                on:click={onCreateNewWorkspace}
+        >
+            Create new
+        </button>
+		<button class="btn variant-ghost-error move-right"
+		on:click={onCreateNewWorkspace}
+		>
+			Delete
+		</button>
+    </header>
+    <div class="logo-cloud grid-cols-3 gap-2 p-4">
+        {#each filteredWorkspaces as workspace}
+            {#if workspace === _selectedWorkspace}
+                <button class="btn logo-item variant-ghost-primary"
+                        on:click={() => selectWorkspace(workspace)}
+                        class:selected={_selectedWorkspace === workspace}
+                >
+                    <span>{workspace}</span>
+                </button>
+            {:else}
+                <button class="btn logo-item variant-ghost-tertiary"
+                        on:click={() => selectWorkspace(workspace)}
+                        class:selected={_selectedWorkspace === workspace}
+                >
+                    <span>{workspace}</span>
+                </button>
+            {/if}
+        {/each}
+    </div>
 </div>
 
 <style>
-    .workspaces {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    .logo-item {
+        border-radius: 0.5rem;
+        padding: 1rem;
+        height: 100px;
     }
     input {
+        border-radius: 0.5rem;
         color: black;
     }
+	.move-right {
+		margin-left: auto;
+	}
 </style>
