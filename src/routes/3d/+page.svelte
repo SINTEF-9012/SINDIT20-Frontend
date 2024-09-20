6c3ed380a3f84c50e70bce7fd4e13d25b2d90055
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-09-20 16:33:43 +0200

    wip: 3d toolbox

--- src/routes/3d/+page.svelte
+++ src/routes/3d/+page.svelte
@@ -1,19 +1,73 @@
 <script lang="ts">
   import { Canvas } from '@threlte/core'
   import Scene from './Scene.svelte'
+  import Toolbox from './Toolbox.svelte'
+
+  let showToolbox = false
+
+  function handleCloseToolbox(event: any) {
+    if ((event.detail.message === 'close') && (showToolbox = true)) {
+      showToolbox = false
+    }
+  }
 </script>
 
-<div class="threlte-canvas">
-  <Canvas>
-    <Scene
-    />
-  </Canvas>
+<div class="three-page">
+  <div class="content-wrapper">
+  {#if showToolbox}
+    <div class="column toolbox bg-gray-300 border-gray-50">
+      <Toolbox on:closeToolbox={handleCloseToolbox} />
+    </div>
+  {/if}
+  <div class="column threlte-canvas" class:full-width={!showToolbox}>
+    <Canvas>
+      <Scene
+      />
+    </Canvas>
+  </div>
+  </div>
 </div>
 
+{#if !showToolbox}
+  <div class="toolbox-button">
+    <button class="btn variant-ghost-success" on:click={() => showToolbox = !showToolbox}>
+      Toolbox
+    </button>
+  </div>
+{/if}
+
 <style>
-  .threlte-canvas {
+  .three-page {
     position: relative;
     height: 100%;
     width: 100%;
+  }
+  .content-wrapper {
+    position: absolute;
+    display: flex;
+    height: 100%;
+    width: 100%;
+  }
+  .column {
+    flex: 1;
+  }
+  .toolbox {
+    flex: 0 0 200px;
+    height: 100%;
+  }
+  .threlte-canvas {
+    height: 100%;
+    width: 100%;
+    display: block;
+  }
+  .toolbox-button {
+    position: fixed;
+    z-index: 1;
+    bottom: 0;
+    left: 1;
+    padding-bottom: 10px;
+  }
+  .threlte-canvas.full-width {
+    flex: 1 1 100%;
   }
 </style>
---
+++ src/routes/3d/Toolbox.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
+    const dispatcher = createEventDispatcher()
+
+    function handleClick() {
+        dispatcher('closeToolbox', {message: 'close'})
+    }
+
+</script>
+
+<div class="toolbox-content flex flex-col text-black">
+    <header class="flex justify-center">
+        <h1 class="text-xl">3D Toolbox</h1>
+    </header>
+    <br />
+    <main class="grid grid-cols-1 gap-4">
+        Toolbox
+    </main>
+    <footer class="flex justify-center">
+        <button class="btn variant-ghost-warning" on:click={handleClick}>
+            Close 3D Toolbox
+        </button>
+    </footer>
+</div>
