0b3170349278086ade19aade118477b831c5585f
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-09-27 16:33:17 +0200

    fix: scale model size

--- src/routes/3d/Scene.svelte
+++ src/routes/3d/Scene.svelte
@@ -10,7 +10,7 @@
 
     const cameraPosition = [20, 20, 20]
     const lightPosition = [0, 20, 20]
-    const gridSize = 5
+    const gridRadius = 10
 
     let dlHelper: DirectionalLightHelper
     let loader = useLoader(GLTFLoader);
@@ -21,6 +21,14 @@
 
     async function loadModel() {
         model = await loader.load(selectedModel.path);
+        const box = new Box3().setFromObject(model.scene);
+        const size = new Vector3();
+        const center = new Vector3();
+        box.getSize(size)
+        box.getCenter(center);
+
+        const maxAxis = Math.max(size.x, size.y, size.z);
+        model.scene.scale.multiplyScalar(gridRadius / maxAxis);
     }
 
     $: if (selectedModel) {
@@ -37,26 +45,27 @@
 <T.PerspectiveCamera
     makeDefault
     position={cameraPosition}
+    target={[0, 0, 0]}
     >
     <OrbitControls />
 </T.PerspectiveCamera>
-<Grid type={'polar'} size={gridSize} />
+<Grid type={'polar'} maxRadius={gridRadius} cellDividers={6} sectionDividers={12}/>
 <T.AmbientLight intensity={0.5} color={0xffffff} />
 <T.HemisphereLight intensity={0.5} color={0xffffff} />
 <T.DirectionalLight
-let:ref
-position={lightPosition} intensity={2} color={0xffffff}
+    let:ref
+    position={lightPosition} intensity={2} color={0xffffff}
 >
-<TransformControls
-    object={ref}
-    on:objectChange={() => dlHelper.update()}
-/>
-<Portal object={model?.scene}>
-    <T.DirectionalLightHelper
-        args={[ref]}
-        bind:ref={dlHelper}
+    <TransformControls
+        object={ref}
+        on:objectChange={() => dlHelper.update()}
     />
-</Portal>
+    <Portal object={model?.scene}>
+        <T.DirectionalLightHelper
+            args={[ref]}
+            bind:ref={dlHelper}
+        />
+    </Portal>
 </T.DirectionalLight>
 
 {#if model}
