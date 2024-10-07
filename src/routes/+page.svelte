8849903d6025f2a10b5560a8b38c4463310e88ee
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-10-07 16:17:10 +0200

    wip: test json editor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,36 +87,51 @@
     <button class="btn variant-outline-warning" on:click={toggleJSONEditor}>Toggle Editor</button>
 </div>
 
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<div class="container mt-2" on:mousemove={handleMouseMove} on:mouseup={handleMouseUp} role="application">
-    <div class="canvas-container">
-        Canvas Content
-    </div>
-    {#if showJSONEditor}
-        <button class="resizer" on:mousedown={handleMouseDown} aria-label="resizer"></button>
-        <div class="json-editor jse-theme-dark">
-            <JSONEditor bind:content mode="text" />
+
+<div class="canvas-page">
+	<div class="toolbox-button-container">
+		<button class="toolbox-button border border-black variant-glass-primary" on:click={() => console.log("hello world")}>
+			<svg class="toolbox-icon" style="transform: rotate(90deg);" viewBox="0 0 24 24">
+				<path d="M12 2L2 22h20L12 2zm-2 16h4v-2h-4v2zm0-4h4v-2h-4v2zm0-4h4V8h-4v2z" fill="currentColor"/>
+			</svg>
+		</button>
+	</div>
+    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+    <div class="container" on:mousemove={handleMouseMove} on:mouseup={handleMouseUp} role="application">
+        <div class="canvas-container">
+            Canvas Content
         </div>
-    {/if}
+        {#if showJSONEditor}
+            <button class="resizer" on:mousedown={handleMouseDown} aria-label="resizer"></button>
+            <div class="json-editor jse-theme-dark">
+                <JSONEditor bind:content mode="text" />
+            </div>
+        {/if}
+    </div>
 </div>
 
 
 
 <style>
     @import 'svelte-jsoneditor/themes/jse-theme-dark.css';
-    .container {
+    .canvas-page {
         display: flex;
         width: 100%;
         height: 90vh;
     }
+    .container {
+        display: flex;
+        height: 90vh;
+        width: 100%;
+        min-width: 100%;
+        max-width: 100%;
+    }
     .json-editor {
         border: 1px solid white;
-        width: 20%;
         min-width: 380px;
     }
     .canvas-container {
         border: 1px solid white;
-        width: 80%;
         min-width:50%;
     }
     .resizer {
@@ -124,4 +139,17 @@
         cursor: col-resize;
         background-color: gray;
     }
+	.toolbox-button-container {
+		display: flex;
+		align-items: stretch;
+		width: 10px;
+	}
+	.toolbox-button {
+		right: 0;
+		width: 10px;
+		height: 100%;
+		padding: 0;
+		box-sizing: border-box;
+		max-width: 10px;
+	}
 </style>
