6e69a84190c2d25ce7e24ff46a152b4379b0bc54
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-10-07 10:00:58 +0200

    wip: json editor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,46 +2,30 @@
 <script lang="ts">
     import { getNodes, createAbstractNode } from '$apis/sindit-backend/kg';
     import { onMount } from 'svelte';
-
-    let data;
-    let error;
+    import { JSONEditor } from 'svelte-jsoneditor'
 
 	const nodeId = "#1234"
     const nodeName = "Node Name"
     const nodeDescription = "Node Description"
 
-
     let isResizing = false;
     let startX: number;
-    let startWidth: number;
-
-    function handleMouseDown(event: MouseEvent) {
-        isResizing = true;
-        startX = event.clientX;
-        startWidth = document.querySelector('.json-editor').offsetWidth;
+    let pageWidth: number;
+    let canvasWidth: number;
+    let editorWidth: number;
+    let showJSONEditor = true;
+    let data: any;
+    let error: string;
+    let content = {
+        text: undefined, // can be used to pass a stringified JSON document instead
+        json: [
+            {a: 1},
+            {a: 2},
+            {a: 3},
+        ]
     }
+    $: console.log(content)
 
-    function handleMouseMove(event: MouseEvent) {
-        if (!isResizing) return;
-        const dx = event.clientX - startX;
-        const newWidth = startWidth + dx;
-        const containerWidth = document.querySelector('.container').offsetWidth;
-        document.querySelector('.json-editor').style.width = `${newWidth}px`;
-        document.querySelector('.canvas-container').style.width = `${containerWidth - newWidth}px`;
-
-    }
-
-    function handleMouseUp() {
-        isResizing = false;
-    }
-
-    onMount(async () => {
-        try {
-            data = await getNodes();
-        } catch (err) {
-            error = err.message;
-        }
-    });
 
     async function handleSubmit() {
         try {
@@ -51,6 +35,45 @@
             error = err.message;
         }
     }
+
+    function handleMouseDown(event: MouseEvent) {
+        isResizing = true;
+        startX = event.clientX;
+        editorWidth = document.querySelector('.json-editor').clientWidth;
+        canvasWidth = document.querySelector('.canvas-container').offsetWidth;
+        pageWidth = document.querySelector('.container').clientWidth;
+    }
+
+    function handleMouseMove(event: MouseEvent) {
+        if (!isResizing) return;
+        const dx = event.clientX - startX;
+        const canvasNewWidth = canvasWidth + dx;
+        const editorNewWidth = pageWidth - canvasNewWidth;
+        document.querySelector('.canvas-container').style.width = `${canvasNewWidth}px`;
+        document.querySelector('.json-editor').style.width = `${editorNewWidth}px`;
+
+    }
+
+    function handleMouseUp() {
+        isResizing = false;
+        editorWidth = document.querySelector('.json-editor').clientWidth;
+        canvasWidth = document.querySelector('.canvas-container').offsetWidth;
+    }
+
+    function toggleJSONEditor() {
+        showJSONEditor = !showJSONEditor;
+        if (!showJSONEditor) {
+            document.querySelector('.canvas-container').style.width = '100%';
+        }
+    }
+
+    onMount(async () => {
+        try {
+            data = await getNodes();
+        } catch (err) {
+            error = err.message;
+        }
+    });
 </script>
 
 {#if error}
@@ -59,22 +82,28 @@
     <p>Data: {JSON.stringify(data)}</p>
 {/if}
 
-<button class="btn variant-outline-success" on:click={handleSubmit}>Submit</button>
+<div class="mt-2">
+    <button class="btn variant-outline-success" on:click={handleSubmit}>Submit</button>
+    <button class="btn variant-outline-warning" on:click={toggleJSONEditor}>Toggle Editor</button>
+</div>
 
-
-<div class="container" on:mousemove={handleMouseMove} on:mouseup={handleMouseUp} role="application">
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<div class="container mt-2" on:mousemove={handleMouseMove} on:mouseup={handleMouseUp} role="application">
     <div class="canvas-container">
         Canvas Content
     </div>
-    <div class="resizer" on:mousedown={handleMouseDown} role="separator"></div>
-    <div class="json-editor">
-        Hello World
-    </div>
+    {#if showJSONEditor}
+        <button class="resizer" on:mousedown={handleMouseDown} aria-label="resizer"></button>
+        <div class="json-editor jse-theme-dark">
+            <JSONEditor bind:content mode="text" />
+        </div>
+    {/if}
 </div>
 
 
 
 <style>
+    @import 'svelte-jsoneditor/themes/jse-theme-dark.css';
     .container {
         display: flex;
         width: 100%;
@@ -82,12 +111,12 @@
     }
     .json-editor {
         border: 1px solid white;
-        width: 50%;
-        min-width: 100px;
+        width: 20%;
+        min-width: 380px;
     }
     .canvas-container {
         border: 1px solid white;
-        width: 50%;
+        width: 80%;
         min-width:50%;
     }
     .resizer {
