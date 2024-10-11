8b7a3a4edf526d6af9ea143671aeb2c6350cc94b
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-10-11 14:00:39 +0200

    fix: add api endpoint call to connections

---
+++ src/apis/sindit-backend/connection.ts
@@ -0,0 +1,18 @@
+
+const API_BASE_URL = import.meta.env.VITE_SINDIT_BACKEND_API
+const API_BASE_ENDPOINT = `${API_BASE_URL}/connection`
+
+
+export async function refreshConnections(): Promise<boolean> {
+    const endpoint = 'refresh';
+    const url = `${API_BASE_ENDPOINT}/${endpoint}`;
+    console.log("refreshConnections", `${url}`)
+    const response = await fetch(`${url}`);
+    if (!response.ok) {
+        throw new Error(`Error performing GET request ${url}`);
+    }  else if (response.status === 200) {
+        return true;
+    } else {
+        return false;
+    }
+}
