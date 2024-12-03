2689d32705af106abc745e644cd245407bae5520
Volker Hoffmann <volker.hoffmann@sintef.no>
2024-12-03 14:28:59 +0100

    test(#41): properly deal with env variables

--- vitest-setup.js
+++ vitest-setup.js
@@ -1,3 +1,16 @@
+import { vi } from 'vitest';
+import dotenv from 'dotenv';
+
+// this is to get the environment variables into $env/dynamic/public
+// so that they're available during the tests
+dotenv.config();
+// console.log(import.meta.env);
+vi.mock('$env/dynamic/public', () => ({
+    env: { 
+        PUBLIC_SINDIT_BACKEND_API: import.meta.env.PUBLIC_SINDIT_BACKEND_API,
+        PUBLIC_SINDIT_BACKEND_API_BASE_URI: import.meta.env.PUBLIC_SINDIT_BACKEND_API_BASE_URI
+    },
+}));
 
 global.window = global.window || {};
 global.window.matchMedia = global.window.matchMedia || function() {
